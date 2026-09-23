35249e9cf49e17c72217abace8e982cc0392f3e3
Abhinav Raj <backupar41@gmail.com>
2026-09-23 21:40:18 +0530

    Added DataItegrityViolation

--- src/main/java/com/example/demo/entity/City.java
+++ src/main/java/com/example/demo/entity/City.java
@@ -4,10 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(
+	    uniqueConstraints = @UniqueConstraint(
+	        columnNames = {"city", "state", "country_code"}
+	    )
+	)
 @Data
 @NoArgsConstructor
 public class City {
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.demo.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -67,5 +68,14 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleInvalidRefreshTokenException(InvalidRefreshTokenException ex) {
 
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", ex.getMessage()));
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<?> handleDataIntegrityViolation(
+	        DataIntegrityViolationException ex) {
+
+	    return ResponseEntity
+	            .status(HttpStatus.CONFLICT)
+	            .body(Map.of("error", "City already exists"));
 	}
 }
